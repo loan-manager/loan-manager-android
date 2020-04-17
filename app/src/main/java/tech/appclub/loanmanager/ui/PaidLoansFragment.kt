bc808bb446818c9e1361e15df2ff9f433000b07f
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-04-17 10:01:56 +0500

    add fragment paid loan logic

--- app/src/main/java/tech/appclub/loanmanager/ui/PaidLoansFragment.kt
+++ app/src/main/java/tech/appclub/loanmanager/ui/PaidLoansFragment.kt
@@ -1,23 +1,37 @@
 package tech.appclub.loanmanager.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import tech.appclub.loanmanager.R
+import tech.appclub.loanmanager.adapters.PaidLoanRecyclerAdapter
+import tech.appclub.loanmanager.databinding.FragmentPaidLoansBinding
+import tech.appclub.loanmanager.viewmodel.LoanViewModel
 
-/**
- * A simple [Fragment] subclass.
- */
 class PaidLoansFragment : Fragment() {
+
+    private lateinit var binding: FragmentPaidLoansBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_paid_loans, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_paid_loans, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val loanViewModel = ViewModelProvider(requireActivity()).get(LoanViewModel::class.java)
+        loanViewModel.paidLoans.observe(requireActivity(), Observer { list ->
+            this.binding.paidLoanRecyclerView.adapter = PaidLoanRecyclerAdapter(list)
+        })
     }
 
 }
