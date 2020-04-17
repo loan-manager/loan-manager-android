1c44bb3a50a4c5ee8ad757300da4314de0045374
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-04-17 11:24:20 +0500

    add history fragment logic

--- app/src/main/java/tech/appclub/loanmanager/ui/HistoryFragment.kt
+++ app/src/main/java/tech/appclub/loanmanager/ui/HistoryFragment.kt
@@ -5,19 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import tech.appclub.loanmanager.R
+import tech.appclub.loanmanager.adapters.AllLoanRecyclerAdapter
+import tech.appclub.loanmanager.adapters.PaidLoanRecyclerAdapter
+import tech.appclub.loanmanager.databinding.FragmentHistoryBinding
+import tech.appclub.loanmanager.viewmodel.LoanViewModel
 
-/**
- * A simple [Fragment] subclass.
- */
 class HistoryFragment : Fragment() {
+
+    private lateinit var binding: FragmentHistoryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_history, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val loanViewModel = ViewModelProvider(requireActivity()).get(LoanViewModel::class.java)
+        loanViewModel.totalLoans.observe(requireActivity(), Observer {
+            this.binding.allRecyclerView.adapter = AllLoanRecyclerAdapter(it, loanViewModel)
+        })
     }
 
 }
