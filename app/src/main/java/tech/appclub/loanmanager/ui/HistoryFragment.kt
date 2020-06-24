e216fab22238da31039c0e24f1685d74cb23df70
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-06-25 00:42:08 +0500

    make following changes in history fragment
    - add recycler view
    - connect view with data binding

---
+++ app/src/main/java/tech/appclub/loanmanager/ui/HistoryFragment.kt
@@ -0,0 +1,24 @@
+package tech.appclub.loanmanager.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import tech.appclub.loanmanager.R
+import tech.appclub.loanmanager.databinding.FragmentHistoryBinding
+
+class HistoryFragment : Fragment() {
+
+    private lateinit var binding: FragmentHistoryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_history, container, false)
+        return binding.root
+    }
+}
