c34bd23116ca84fba96a75d7914e7dbdd161f0dd
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-06-25 21:51:42 +0500

    add visibility for delete all menu action

--- app/src/main/java/tech/appclub/loanmanager/ui/PaidLoansFragment.kt
+++ app/src/main/java/tech/appclub/loanmanager/ui/PaidLoansFragment.kt
@@ -18,6 +18,7 @@ class PaidLoansFragment : Fragment() {
     private lateinit var loanViewModel: LoanViewModel
     private lateinit var binding: FragmentPaidLoansBinding
     private var size: Int? = null
+    private var listSize: Int = -1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,11 +42,18 @@ class PaidLoansFragment : Fragment() {
                 this.binding.emptyMsg.visibility = View.GONE
             }
             this.binding.paidLoanRecyclerView.adapter = PaidLoanRecyclerAdapter(list)
+            listSize = list.size
         })
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.general_menu, menu)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        requireActivity().invalidateOptionsMenu()
+        menu.findItem(R.id.action_delete_all).isVisible = listSize != 0
+        super.onPrepareOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
